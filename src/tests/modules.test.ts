d5c4a5fc2a4b7fcc3f284901e0ac145f3cf9c1bc
morde <mordechai.hadad01@gmail.com>
2023-12-05 12:44:18 +0200

    added: more tests

--- src/tests/modules.test.ts
+++ src/tests/modules.test.ts
@@ -1,7 +1,13 @@
-import { display, sortItems } from "$lib/modules.js";
+import {
+    boldMatchedChars,
+    boldUnmatchedChars,
+    display,
+    filterOnChange,
+    sortItems,
+} from "$lib/modules.js";
 import { expect, describe, test } from "vitest";
 
-describe("sortItems function", () => {
+describe("sortItems function tests", () => {
     test("should sort string array in ascending order", () => {
         const input = ["b", "a", "c"];
         const expected = ["a", "b", "c"];
@@ -31,7 +37,7 @@ describe("sortItems function", () => {
     });
 });
 
-describe("display function", () => {
+describe("display function tests", () => {
     test("should return the specified field value", () => {
         const input = { name: "Bob", age: "69" };
         const expected = "Bob";
@@ -44,5 +50,109 @@ describe("display function", () => {
         const expected = "John";
         const result = display("name", input);
         expect(result).toEqual(expected);
+    });
+});
+
+describe("boldUnmatchedChars function tests", () => {
+    test("should return the word with unmatched characters bolded", () => {
+        const word = "hello";
+        const value = "ell";
+        const result = boldUnmatchedChars(word, value);
+        expect(result).toBe("<b>h</b>ell<b>o</b>");
+    });
+
+    test("should return the word as is if all characters match", () => {
+        const word = "hello";
+        const value = "hello";
+        const result = boldUnmatchedChars(word, value);
+        expect(result).toBe("hello");
+    });
+
+    test("should return the word with all characters bolded if no characters match", () => {
+        const word = "hello";
+        const value = "world";
+        const result = boldUnmatchedChars(word, value);
+        expect(result).toBe("<b>h</b><b>e</b><b>l</b><b>l</b><b>o</b>");
+    });
+});
+
+describe("boldMatchedChars function tests", () => {
+    test("all characters match", () => {
+        const result = boldMatchedChars("test", "test");
+        expect(result).toBe("<b>test</b>");
+    });
+
+    test("some characters match", () => {
+        const result = boldMatchedChars("testing", "test");
+        console.log(result);
+        expect(result).toBe("<b>test</b>ing");
+    });
+
+    test("no characters match", () => {
+        const result = boldMatchedChars("test", "example");
+        expect(result).toBe("test");
+    });
+});
+
+describe("filterOnChange function tests", () => {
+    const itemsStringOnly = ["Apple", "Banana", "Orange"];
+    const itemsObject = [
+        { name: "Apple" },
+        { name: "Banana" },
+        { name: "Orange" },
+    ];
+
+    test("should filter string items based on value", () => {
+        const displayField = "";
+        const value = "a";
+        const filteredResult = filterOnChange(
+            displayField,
+            itemsStringOnly,
+            value
+        );
+        expect(filteredResult).toEqual(["Apple", "Banana", "Orange"]);
+    });
+
+    test("should filter object items based on displayField and value", () => {
+        const displayField = "name";
+        const value = "a";
+        const filteredResult = filterOnChange(displayField, itemsObject, value);
+        expect(filteredResult).toEqual([
+            { name: "Apple" },
+            { name: "Banana" },
+            { name: "Orange" },
+        ]);
+    });
+
+    test("should handle empty input array", () => {
+        const displayField = "name";
+        const value = "a";
+        const filteredResult = filterOnChange(displayField, [], value);
+        expect(filteredResult).toEqual([]);
+    });
+
+    test("should handle case-insensitive search", () => {
+        const displayField = "name";
+        const value = "oR";
+        const filteredResult = filterOnChange(displayField, itemsObject, value);
+        expect(filteredResult).toEqual([{ name: "Orange" }]);
+    });
+
+    test("should return orange if given o", () => {
+        const displayField = "";
+        const value = "o";
+        const filteredResult = filterOnChange(
+            displayField,
+            itemsStringOnly,
+            value
+        );
+        expect(filteredResult).toEqual(["Orange"]);
+    });
+
+    test("should return an empty array for non-matching value", () => {
+        const displayField = "name";
+        const value = "xyz";
+        const filteredResult = filterOnChange(displayField, itemsObject, value);
+        expect(filteredResult).toEqual([]);
     });
 });
