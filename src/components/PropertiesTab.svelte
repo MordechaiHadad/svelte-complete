4c102527bae6306000b71267815fb5ef0721b7f5
morde <mordechai.hadad01@gmail.com>
2023-12-24 14:04:17 +0200

    make table gooder

--- src/components/PropertiesTab.svelte
+++ src/components/PropertiesTab.svelte
@@ -13,11 +13,11 @@
             " "
         ),
         tableHeaders:
-            "border-b border-neutral-500 dark:border-neutral-500 bg-neutral-200 dark:bg-neutral-800 py-2 px-4 text-start".split(
+            "border-b text-sm md:text-base border-neutral-500 dark:border-neutral-500 bg-neutral-200 dark:bg-neutral-800 py-2 px-2 lg:px-4 text-start".split(
                 " "
             ),
         tableData:
-            "border-y border-neutral-500 dark:border-neutral-500 px-4 py-2 pt-4 align-top".split(
+            "border-y border-neutral-500 text-sm md:text-base dark:border-neutral-500 px-2 lg:px-4 py-2 lg:pt-4 align-top".split(
                 " "
             ),
     };
@@ -52,7 +52,7 @@
     <div class="flex flex-col gap-6">
         <h1 class="text-xl md:text-2xl">Component Properties</h1>
         <div
-            class="max-w-xs overflow-auto lg:max-w-4xl rounded-md border border-neutral-500 dark:bg-neutral-500"
+            class="max-w-xs overflow-auto md:max-w-2xl lg:max-w-4xl rounded-md border border-neutral-500 dark:bg-neutral-500"
         >
             <table id="props"></table>
         </div>
@@ -60,7 +60,7 @@
     <div class="flex flex-col gap-4">
         <h1 class="text-xl md:text-2xl">Component CSS Variables</h1>
         <div
-            class="max-w-xs overflow-auto lg:max-w-4xl  rounded-md border border-neutral-500 dark:bg-neutral-500"
+            class="max-w-xs overflow-auto md:max-w-2xl lg:max-w-4xl rounded-md border border-neutral-500 dark:bg-neutral-500"
         >
             <table id="vars"></table>
         </div>
